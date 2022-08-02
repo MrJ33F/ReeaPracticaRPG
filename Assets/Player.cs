5e102670d1d677ce34ec7af9ad0e05dcb21f5297
ncsilla <nagy.csilla.b@gmail.com>
2022-08-02 13:11:42 +0300

    second day

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int level = 1;
+    public int health = 40;
+
+    public void P()
+    {
+        System.Console.WriteLine("kk");
+    }
+
+    public void SavePlayer()
+    {
+        Save.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = Save.Load();
+        level = data.level;
+        health = data.health;
+    }
+}
