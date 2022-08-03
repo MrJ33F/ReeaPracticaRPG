3b190b309087dbbe4b67744caa5ad58fdb237069
PaulStefG <paulstefangergely@gmail.com>
2022-08-03 11:59:40 +0300

    up

--- Assets/Scripts/Travel.cs
+++ Assets/Scripts/Travel.cs
@@ -7,7 +7,17 @@ public class Travel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("TempleExit")){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        if (collision.gameObject.CompareTag("CaveExit")){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+        if (collision.gameObject.CompareTag("CaveEnter")){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        if (collision.gameObject.CompareTag("TempleEnter")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
+
 }
