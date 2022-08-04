493fedd0081568afa75084835d61114f121c0fc4
PaulStefG <paulstefangergely@gmail.com>
2022-08-04 10:19:43 +0300

    COMM

---
+++ Assets/Scripts/Map/Travel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Travel : MonoBehaviour
+{
+    public GameObject templeKey;
+    public GameObject settingsBut;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("TempleExit")){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        if (collision.gameObject.CompareTag("CaveExit")){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+        if (collision.gameObject.CompareTag("CaveEnter")){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
+        if (collision.gameObject.CompareTag("TempleEnter")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (collision.gameObject.CompareTag("TempleKey")){
+            templeKey.SetActive(true);
+            settingsBut.SetActive(false);
+        }
+    }
+
+}
